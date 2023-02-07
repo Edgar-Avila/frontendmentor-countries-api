c6f9dba989ebe363b728d651eded4ecc19a6c02c
Edgar <avila.agramonte.edg@gmail.com>
2023-02-07 13:21:25 -0500

    fix: Prerender error because of localstorage

--- src/store/theme.js
+++ src/store/theme.js
@@ -1,7 +1,8 @@
+import { useEffect, useState } from "react"
 import { create } from "zustand"
 import { persist } from "zustand/middleware"
 
-export const useThemeStore = create(
+export const useThemeStorePersist = create(
     persist(
         (set, get) => ({
             isDark: false,
@@ -14,3 +15,18 @@ export const useThemeStore = create(
         }
     )
 )
+
+export const useThemeStore = (selector = state => state) => {
+    const state = useThemeStorePersist()
+
+    const [isDark, setIsDark] = useState(false)
+
+    useEffect(() => {
+        setIsDark(state.isDark)
+    }, [state.isDark])
+
+    return selector({
+        toggleTheme: state.toggleTheme,
+        isDark
+    })
+}
